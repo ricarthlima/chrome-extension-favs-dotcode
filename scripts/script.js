bac6d00c96efc64505727c299b152b02c9d6ed25
Ricarth Lima <rrsl@cin.ufpe.br>
2021-05-13 20:43:13 -0300

    página de categorias implementada

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,9 @@ var urlIconCurrent = "";
 
 var formTitle = document.getElementById("form-title");
 
+var listFavs = [];
+var listCategory = [];
+
 window.onload = function () {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
         let title = tabs[0].title;
@@ -72,6 +75,53 @@ function saveFav() {
 function getFavs() {
     var divCategoryContainer = document.getElementById("category-container");
     chrome.storage.sync.get("STORAGE_KEY", function (result) {
-        divCategoryContainer.innerHTML = result["STORAGE_KEY"];
+        var tempListFavs = result["STORAGE_KEY"].split("\n");
+
+        listFavs = [];
+        listCategory = [];
+        for (var i = 0; i < tempListFavs.length; i++) {
+            let element = JSON.parse(tempListFavs[i]);
+            let category = element["category"];
+
+            listFavs.push(element);
+            if (!listCategory.includes(category)) {
+                listCategory.push(category);
+            }
+        }
+
+
+        populateSearchPage();
+        //divCategoryContainer.innerHTML = listCategory;
     });
+}
+
+function populateSearchPage() {
+    var divCategoryContainer = document.getElementById("category-container");
+    divCategoryContainer.innerHTML = "";
+
+    for (var i = 0; i < listCategory.length; i++) {
+        let categoryName = listCategory[i];
+
+        var categoryDiv = document.createElement("DIV");
+        categoryDiv.id = "category" + i.toString();
+        categoryDiv.classList.add("col-4");
+
+        categoryDiv.innerHTML =
+            '<div class="category-header">' +
+            '<img src="assets/folder.png" height="30">' +
+            '</div>' +
+            '<div class="category-body">' +
+            categoryName +
+            '</div>';
+
+        divCategoryContainer.appendChild(categoryDiv);
+
+        document.getElementById("category" + i.toString()).addEventListener("click", function () {
+            showListByCategory(categoryName);
+        });
+    }
+}
+
+function showListByCategory(categoryName) {
+    console.log(categoryName);
 }
